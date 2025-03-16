916e2e1f6c527f052aee427f9633db5f55975988
Bruno Valmiki Vargas Balderas <80133864+bruno1113@users.noreply.github.com>
2025-03-16 09:17:29 -0600

    Add files via upload

---
+++ recursos/js/servicios.js
@@ -0,0 +1,235 @@
+function boton1() {
+    var btn = document.getElementById("div-1");
+    if (btn.style.display === "none") {
+      btn.style.display = "block";
+    } else {
+      btn.style.display = "none";
+    }
+} 
+
+function boton2() {
+    var btn2 = document.getElementById("div-2");
+    if (btn2.style.display === "none") {
+      btn2.style.display = "block";
+    } else {
+      btn2.style.display = "none";
+    }
+}
+
+function boton3() {
+  var btn3 = document.getElementById("div-3");
+  if (btn3.style.display === "none") {
+    btn3.style.display = "block";
+  } else {
+    btn3.style.display = "none";
+  }
+}
+
+function boton4() {
+  var btn4 = document.getElementById("div-4");
+  if (btn4.style.display === "none") {
+    btn4.style.display = "block";
+  } else {
+    btn4.style.display = "none";
+  }
+}
+
+function boton5() {
+  var btn5 = document.getElementById("div-5");
+  if (btn5.style.display === "none") {
+    btn5.style.display = "block";
+  } else {
+    btn5.style.display = "none";
+  }
+}
+
+function boton6() {
+  var btn6 = document.getElementById("div-6");
+  if (btn6.style.display === "none") {
+    btn6.style.display = "block";
+  } else {
+    btn6.style.display = "none";
+  }
+}
+
+function botonopen1() {
+  var btn = document.getElementById("div-interno1");
+  var bntopen = document.getElementById("open-button");
+  var bntclose = document.getElementById("close-button");
+
+  if (btn.style.display === "none") {
+    btn.style.display = "block";
+    bntopen.style.display = "none";
+    bntclose.style.display = "block";
+  } else {
+    btn.style.display = "none";
+    bntopen.style.display = "block";
+  }
+}
+
+function botonclose1() {
+  var btn = document.getElementById("div-interno1");
+  var bntclose = document.getElementById("close-button");
+  var bntopen = document.getElementById("open-button");
+
+  if (btn.style.display === "none") {
+    btn.style.display = "block";
+    bntclose.style.display = "block";
+  } else {
+    btn.style.display = "none";
+    bntclose.style.display = "none";
+    bntopen.style.display = "block";
+  }
+}
+
+
+
+function botonopen2() {
+  var btn = document.getElementById("div-interno2");
+  var bntopen = document.getElementById("open-button2");
+  var bntclose = document.getElementById("close-button2");
+
+  if (btn.style.display === "none") {
+    btn.style.display = "block";
+    bntopen.style.display = "none";
+    bntclose.style.display = "block";
+  } else {
+    btn.style.display = "none";
+    bntopen.style.display = "block";
+  }
+}
+
+function botonclose2() {
+  var btn = document.getElementById("div-interno2");
+  var bntclose = document.getElementById("close-button2");
+  var bntopen = document.getElementById("open-button2");
+
+  if (btn.style.display === "none") {
+    btn.style.display = "block";
+    bntclose.style.display = "block";
+  } else {
+    btn.style.display = "none";
+    bntclose.style.display = "none";
+    bntopen.style.display = "block";
+  }
+} 
+
+function botonopen3() {
+  var btn = document.getElementById("div-interno3");
+  var bntopen = document.getElementById("open-button3");
+  var bntclose = document.getElementById("close-button3");
+
+  if (btn.style.display === "none") {
+    btn.style.display = "block";
+    bntopen.style.display = "none";
+    bntclose.style.display = "block";
+  } else {
+    btn.style.display = "none";
+    bntopen.style.display = "block";
+  }
+}
+
+function botonclose3() {
+  var btn = document.getElementById("div-interno3");
+  var bntclose = document.getElementById("close-button3");
+  var bntopen = document.getElementById("open-button3");
+
+  if (btn.style.display === "none") {
+    btn.style.display = "block";
+    bntclose.style.display = "block";
+  } else {
+    btn.style.display = "none";
+    bntclose.style.display = "none";
+    bntopen.style.display = "block";
+  }
+}
+
+function botonclose4() {
+  var btn = document.getElementById("div-interno4");
+  var bntclose = document.getElementById("close-button4");
+  var bntopen = document.getElementById("open-button4");
+
+  if (btn.style.display === "none") {
+    btn.style.display = "block";
+    bntclose.style.display = "block";
+  } else {
+    btn.style.display = "none";
+    bntclose.style.display = "none";
+    bntopen.style.display = "block";
+  }
+}
+
+function botonopen4() {
+  var btn = document.getElementById("div-interno4");
+  var bntopen = document.getElementById("open-button4");
+  var bntclose = document.getElementById("close-button4");
+
+  if (btn.style.display === "none") {
+    btn.style.display = "block";
+    bntopen.style.display = "none";
+    bntclose.style.display = "block";
+  } else {
+    btn.style.display = "none";
+    bntopen.style.display = "block";
+  }
+}
+
+function botonopen5() {
+  var btn = document.getElementById("div-interno5");
+  var bntopen = document.getElementById("open-button5");
+  var bntclose = document.getElementById("close-button5");
+
+  if (btn.style.display === "none") {
+    btn.style.display = "block";
+    bntopen.style.display = "none";
+    bntclose.style.display = "block";
+  } else {
+    btn.style.display = "none";
+    bntopen.style.display = "block";
+  }
+}
+
+function botonclose5() {
+  var btn = document.getElementById("div-interno5");
+  var bntclose = document.getElementById("close-button5");
+  var bntopen = document.getElementById("open-button5");
+
+  if (btn.style.display === "none") {
+    btn.style.display = "block";
+    bntclose.style.display = "block";
+  } else {
+    btn.style.display = "none";
+    bntclose.style.display = "none";
+    bntopen.style.display = "block";
+  }
+}
+
+function botonopen6() {
+  var btn = document.getElementById("div-interno6");
+  var bntopen = document.getElementById("open-button6");
+  var bntclose = document.getElementById("close-button6");
+
+  if (btn.style.display === "none") {
+    btn.style.display = "block";
+    bntopen.style.display = "none";
+    bntclose.style.display = "block";
+  } else {
+    btn.style.display = "none";
+    bntopen.style.display = "block";
+  }
+}
+
+function botonclose6() {
+  var btn = document.getElementById("div-interno6");
+  var bntclose = document.getElementById("close-button6");
+  var bntopen = document.getElementById("open-button6");
+
+  if (btn.style.display === "none") {
+    btn.style.display = "block";
+    bntclose.style.display = "block";
+  } else {
+    btn.style.display = "none";
+    bntclose.style.display = "none";
+    bntopen.style.display = "block";
+  }
+}
